35d04836de2b38287fd9a21e15d31f01ad522f5f
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-05-27 04:32:23 +0900

    webApiを作成

--- GoogleAppScript/src/index.ts
+++ GoogleAppScript/src/index.ts
@@ -1,3 +1,20 @@
-global.doGet = () => {
+global.doGet = (e: any) => {
+  //リクエストパラメータ名"text"の値を取得する
   Logger.log(`Hello World`);
+  var text = e.parameter.text;
+  var value;
+  if (text) {
+      value = "You say " + text;
+  } else {
+      value = "Please say something!";
+  }
+  var result = {
+      message: value
+  }
+  var out = ContentService.createTextOutput();
+  //Mime TypeをJSONに設定
+  out.setMimeType(ContentService.MimeType.JSON);
+  //JSONテキストをセットする
+  out.setContent(JSON.stringify(result));
+  return out;
 }
