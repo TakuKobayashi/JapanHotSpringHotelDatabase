b9593c866d3bce563ff06316413a2b307d24ca89
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-06-08 01:28:01 +0900

    spreadsheetの情報をJSONで取得できるようにした

--- GoogleAppScript/src/index.ts
+++ GoogleAppScript/src/index.ts
@@ -1,26 +1,30 @@
 global.doGet = (e: any) => {
+  // e.parameterでURL QueryのObejctが取得できる
   const targetSpreadSheet = SpreadsheetApp.openById("1XsetLCeR4-Q1ntWFRFAWnuXhZK3C23RQgWBTTs7eFW0");
+  const resultObject = {}
   console.log(targetSpreadSheet.getName())
   for(const sheet of targetSpreadSheet.getSheets()){
-    const dataRange  = sheet.getDataRange();
+    const resultJsonObjects = [];
+    const dataRange = sheet.getDataRange();
+    console.log(dataRange.getWidth);
+    const data = dataRange.getValues();
+    for(let row = 1;row < data.length;++row){
+      const sheetData = {};
+      const keys = data[0];
+      for(let column = 0;column < keys.length;++column){
+        sheetData[keys[column]] = data[row][column];
+      }
+      resultJsonObjects.push(sheetData);
+    }
+    resultObject[sheet.getSheetName()] = resultJsonObjects;
     console.log(sheet.getSheetName());
     console.log(dataRange);
     console.log(dataRange.getValues());
-  }
-  //リクエストパラメータ名"text"の値を取得する
-  var value;
-  if (e.parameter) {
-      value = "You say " + JSON.stringify(e.parameter);
-  } else {
-      value = "Please say something!";
-  }
-  var result = {
-      message: value
   }
   var out = ContentService.createTextOutput();
   //Mime TypeをJSONに設定
   out.setMimeType(ContentService.MimeType.JSON);
   //JSONテキストをセットする
-  out.setContent(JSON.stringify(result));
+  out.setContent(JSON.stringify(resultObject));
   return out;
 }
