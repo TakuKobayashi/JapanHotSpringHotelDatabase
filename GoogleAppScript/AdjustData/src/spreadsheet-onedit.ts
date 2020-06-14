e7f54098a21957bf734eccf772a4225cdf5a91fc
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2020-06-15 05:22:10 +0900

    うまくいったのかわからないがデータの自動整形とかを入れてみた

--- GoogleAppScript/AdjustData/src/spreadsheet-onedit.ts
+++ GoogleAppScript/AdjustData/src/spreadsheet-onedit.ts
@@ -9,6 +9,22 @@ function onEdit(event: GoogleAppsScript.Events.SheetsOnEdit): void {
   //編集されたセルの値を取得したい場合
   Logger.log(event.value);
   if(event.range){
+    // getRange(row, column, numRows)
+    const targetSheet = event.range.getSheet();
+    const keyNumberPairs = getKeyNumberPairs(targetSheet);
+    Logger.log(keyNumberPairs);
+
+    // データを自動整形
+    const data = event.range.getValues();
+    for(let row = 0; row < data.length;++row){
+      for(let column = 0;column < data[row];++column){
+        if(data[row][column]){
+          data[row][column] = normalize(data[row][column].toString())
+        }
+      }
+    }
+    event.range.setValues(data);
+
     Logger.log(event.range.getColumn().toString());
     Logger.log(event.range.getRow().toString());
     Logger.log(event.range.getWidth().toString());
@@ -20,16 +36,26 @@ function onEdit(event: GoogleAppsScript.Events.SheetsOnEdit): void {
   Logger.log(event.oldValue);
 }
 
-function updateLatLonRowSheet(sheet: any, rowNumber: number, sheetData: any): any {
-  const resultData = {...sheetData}
-  if(!sheetData.lat || !sheetData.lon){
-    resultData.address = sheetData.address.normalize("NFKC")
-    const responses = convertGeocode(sheetData);
-    resultData.lat = responses[0].geometry.location.lat;
-    resultData.lon = responses[0].geometry.location.lng;
-    const dataArr = Object.values(resultData)
-    // spreadsheetのデータの更新
-    sheet.getRange(rowNumber + 1, 1, 1, dataArr.length).setValues([dataArr]);
+function getKeyNumberPairs(targetSheet: GoogleAppsScript.Spreadsheet.Sheet): { [s: string]: number } {
+  const keyNumberPairs: { [s: string]: number } = {}
+  const headerRange = targetSheet.getRange(1, 1, 1, targetSheet.getLastColumn());
+  const headerValues = headerRange.getValues();
+  Logger.log(headerValues);
+  if(headerValues[0]){
+    for(let i = 0;i < headerValues[0].length;++i){
+      keyNumberPairs[headerValues[0][i]] = i + 1;
+    }
   }
-  return resultData;
+  return keyNumberPairs;
+}
+
+function normalize(word: string): string {
+  return word.normalize("NFKC")
+}
+
+function convertGeocode(address: string): any {
+  const geocoder = Maps.newGeocoder();
+  geocoder.setLanguage('ja');
+  const responses = geocoder.geocode(address);
+  return responses.results;
 }
