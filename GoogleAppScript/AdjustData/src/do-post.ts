019eb077f8b8dac7559b2f8c26555e68b49598d8
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2020-06-15 03:01:53 +0900

    doGet, doPost両方の検証ができました

--- GoogleAppScript/AdjustData/src/do-post.ts
+++ GoogleAppScript/AdjustData/src/do-post.ts
@@ -1,8 +1,17 @@
-function onPost(e: any): GoogleAppsScript.Content.TextOutput {
+function doGet(e: any): GoogleAppsScript.Content.TextOutput {
+  Logger.log("doGet");
   Logger.log(e.parameter);
+  const output = ContentService.createTextOutput();
+  output.setMimeType(ContentService.MimeType.JSON);
+  output.setContent(JSON.stringify({ message: "doGet success!" }));
+  return output;
+}
+
+function doPost(e: any): GoogleAppsScript.Content.TextOutput {
+  Logger.log("doPost");
   Logger.log(e.postData.getDataAsString());
   const output = ContentService.createTextOutput();
   output.setMimeType(ContentService.MimeType.JSON);
-  output.setContent(JSON.stringify({ message: "success!" }));
+  output.setContent(JSON.stringify({ message: "doPost success! :" + e.postData.getDataAsString() }));
   return output;
 }
