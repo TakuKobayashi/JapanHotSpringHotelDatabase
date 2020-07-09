341fc2148ce12339d3c0f27021aa57a7184eae65
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2020-07-10 01:06:56 +0900

    正規表現をまとめたものを導入しました

--- serverless/src/common/html-scraping.ts
+++ serverless/src/common/html-scraping.ts
@@ -2,63 +2,14 @@ import axios from 'axios';
 import { isURL, normalizeURL } from './util';
 import cheerio, { CheerioStatic } from 'cheerio';
 import { uniq } from 'lodash';
+import { phoneNumberRegExp, japanAddressRegExp } from './regexp-components';
 const addressableUrl = require('url');
-
-const todoufukenList = [
-  '北海道',
-  '青森県',
-  '岩手県',
-  '秋田県',
-  '山形県',
-  '宮城県',
-  '福島県',
-  '群馬県',
-  '栃木県',
-  '茨城県',
-  '埼玉県',
-  '東京都',
-  '千葉県',
-  '神奈川県',
-  '新潟県',
-  '長野県',
-  '富山県',
-  '石川県',
-  '福井県',
-  '静岡県',
-  '山梨県',
-  '愛知県',
-  '岐阜県',
-  '滋賀県',
-  '三重県',
-  '奈良県',
-  '和歌山県',
-  '京都府',
-  '大阪府',
-  '兵庫県',
-  '岡山県',
-  '広島県',
-  '鳥取県',
-  '島根県',
-  '山口県',
-  '香川県',
-  '徳島県',
-  '高知県',
-  '愛媛県',
-  '福岡県',
-  '佐賀県',
-  '長崎県',
-  '大分県',
-  '熊本県',
-  '宮崎県',
-  '鹿児島県',
-  '沖縄県',
-];
 
 export async function analize(urlString: string) {
   const rootUrl = addressableUrl.parse(urlString);
   const $ = await loadAndParseHTMLfromCheerio(rootUrl.href);
   const trimedText = $.text().trim();
-  const phoneNumbers = trimedText.match(/0\d{2,3}-\d{1,4}-\d{4}/g);
+  const phoneNumbers = trimedText.match(phoneNumberRegExp('g'));
   const linkUrls = [];
   const mailAddresses = [];
   $('a').each((i, elem) => {
@@ -85,14 +36,7 @@ export async function analize(urlString: string) {
   const texts = trimedText.split(/\s/);
   for (const text of texts) {
     if (text.length > 0) {
-      const regexp = new RegExp(
-        '(' +
-          todoufukenList.join('|') +
-          ')' +
-          '((?:旭川|伊達|石狩|盛岡|奥州|田村|南相馬|那須塩原|東村山|武蔵村山|羽村|十日町|上越|富山|野々市|大町|蒲郡|四日市|姫路|大和郡山|廿日市|下松|岩国|田川|大村|宮古|富良野|別府|佐伯|黒部|小諸|塩尻|玉野|周南)市|(?:余市|高市|[^市]{2,3}?)郡(?:玉村|大町|.{1,5}?)[町村]|(?:.{1,4}市)?[^町]{1,4}?区|.{1,7}?[市町村])' +
-          '(.+)',
-        'g',
-      );
+      const regexp = japanAddressRegExp('g');
       const matchedAddresses = text.match(regexp);
       if (matchedAddresses && matchedAddresses.length > 0) {
         for (const matchedAddress of matchedAddresses) {
@@ -109,7 +53,6 @@ export async function analize(urlString: string) {
     }
   });
   const bgUrls = $.html().match(/background[-image]*:[\s]*url\(["|\']+(.*)["|\']+\)/g);
-  console.log($.html());
   return {
     phoneNumbers: uniq(phoneNumbers),
     addresses: uniq(addresses),
--- serverless/src/common/util.ts
+++ serverless/src/common/util.ts
@@ -1,18 +1,12 @@
+import { urlRegExp } from './regexp-components';
+const addressableUrl = require('url');
+const path = require('path');
+
 export function isURL(str: string): boolean {
-  const pattern = new RegExp(
-    '^(https?:\\/\\/)?' + // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$',
-    'i',
-  ); // fragment locator
+  const pattern = urlRegExp('i');
   return pattern.test(str);
 }
 
-const addressableUrl = require('url');
-const path = require('path');
 export function normalizeURL(srcURLString: string, orgURLString: string): string {
   const aUrl = addressableUrl.parse(srcURLString);
   if (!aUrl.protocol) {
